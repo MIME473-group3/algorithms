c96fafcd718d2bdec5d555120be68399ea0268b1
Adam Kosiorek <kosiorek.adam@gmail.com>
2014-04-30 17:56:28 +0200

    cleaned treemap header and src file

--- src/tree/asd.cc
+++ src/tree/asd.cc
@@ -1,31 +1,31 @@
 /** 
-@file asd.cc
+ @file asd.cc
 
-Plik do modyfikacji w w ramach cwiczenia 4 z AISDI.
-Zawiera niekompletne implementacje metod klasy TreeMap.
-Jest tez prosta funkcja testujaca (void test()), ktora
-jest wolana w funkcji main. Mozna w niej zaimplementowac
-wlasne testy.
-NALEZY ZMODYFIKOWAC I UZUPELNIC CIALA METOD KLASY TreeMap.
-    
-@author
-Pawel Cichocki, Michal Nowacki
+ Plik do modyfikacji w w ramach cwiczenia 4 z AISDI.
+ Zawiera niekompletne implementacje metod klasy TreeMap.
+ Jest tez prosta funkcja testujaca (void test()), ktora
+ jest wolana w funkcji main. Mozna w niej zaimplementowac
+ wlasne testy.
+ NALEZY ZMODYFIKOWAC I UZUPELNIC CIALA METOD KLASY TreeMap.
 
-@date  
-last revision
-- 2005.12.01 Pawel Cichocki: TreeNodeDetail class
-- 2005.12.01 Michal Nowacki: lab #4
-- 2005.11.17 Michal Nowacki: lab #3, copy operator and constructor
-- 2005.11.04 Pawel Cichocki: copied comments from the header
-- 2005.11.03 Pawel Cichocki: const_iterator done properly now
-- 2005.10.27 Pawel Cichocki: cosmetic changes
-- 2005.10.26 Michal Nowacki: removed some method bodies
-- 2005.10.25 Pawel Cichocki: wrote it
+ @author
+ Pawel Cichocki, Michal Nowacki
 
-COPYRIGHT:
-Copyright (c) 2005 Instytut Informatyki, Politechnika Warszawska
-ALL RIGHTS RESERVED
-*******************************************************************************/
+ @date
+ last revision
+ - 2005.12.01 Pawel Cichocki: TreeNodeDetail class
+ - 2005.12.01 Michal Nowacki: lab #4
+ - 2005.11.17 Michal Nowacki: lab #3, copy operator and constructor
+ - 2005.11.04 Pawel Cichocki: copied comments from the header
+ - 2005.11.03 Pawel Cichocki: const_iterator done properly now
+ - 2005.10.27 Pawel Cichocki: cosmetic changes
+ - 2005.10.26 Michal Nowacki: removed some method bodies
+ - 2005.10.25 Pawel Cichocki: wrote it
+
+ COPYRIGHT:
+ Copyright (c) 2005 Instytut Informatyki, Politechnika Warszawska
+ ALL RIGHTS RESERVED
+ *******************************************************************************/
 
 #include <assert.h>
 #include <algorithm>
@@ -42,275 +42,148 @@ ALL RIGHTS RESERVED
 class TreeMapDetail //Helper
 {
 protected:
-   friend class TreeMap;
+	friend class TreeMap;
 
-   /// Stupid example of a method that modifies a protected field in 
-   /// the TreeMap class. Feel free to remove this method or add new
-   /// ones here.
-   static void erase(TreeMap* tm, const TreeMap::Key& k)
-   {
-      tm->root=NULL; // we just modified a protected field in tree map
-   }
+	/// Stupid example of a method that modifies a protected field in
+	/// the TreeMap class. Feel free to remove this method or add new
+	/// ones here.
+	static void erase(TreeMap* tm, const TreeMap::K& k) {
+		tm->root = NULL; // we just modified a protected field in tree map
+	}
 };
 
 //////////////////////////////////////////////////////////////////////////////
 // TreeMap and TreeMap::iterator methods
 //////////////////////////////////////////////////////////////////////////////
 
-TreeMap::TreeMap()
-{
-   root = NULL;
-};
-
-/// Content of existing TreeMap object is copied into the new object. 
-TreeMap::TreeMap( const TreeMap& m )
-{
-   ///@todo Implement this
-};
-
-TreeMap::~TreeMap()
-{
-   clear();
-};
-
-// Inserts an element into the map.
-// @returns A pair whose bool component is true if an insertion was
-//          made and false if the map already contained an element
-//          associated with that key, and whose iterator component coresponds to
-//          the address where a new element was inserted or where the element
-//          was already located.
-std::pair<TreeMap::iterator, bool> TreeMap::insert(const std::pair<Key, Val>& entry)
-{
-   ///@todo Finnish this
-   iterator i;
-   for(i=begin(); i!=end(); ++i)
-   {
-      if(i->first==entry.first)
-        return std::make_pair(i, (bool)false);
-   }
-        return std::make_pair(i, (bool)false);
+TreeMap::TreeMap() {
+	root = NULL;
 }
 
-// Inserts an element into the map.
-// This method assumes there is no value asociated with
-// such a key in the map.
-
-TreeMap::iterator TreeMap::unsafe_insert(const std::pair<Key, Val>& entry)
-{
-   ///@todo  Finnish this
-   assert(0);
-   return iterator(root);
+TreeMap::TreeMap(const TreeMap& m) {
 }
 
-// Returns an iterator addressing the location of the entry in the map
-// that has a key equivalent to the specified one or the location succeeding the
-// last element in the map if there is no match for the key.
-TreeMap::iterator TreeMap::find(const Key& k)
-{
-   ///@todo Implement this
-   assert(0);
-   return end();
+TreeMap::~TreeMap() {
+	clear();
 }
 
-TreeMap::const_iterator TreeMap::find(const Key& k) const
-{
-   ///@todo Implement this
-   assert(0);
-   return end();
+std::pair<TreeMap::iterator, bool> TreeMap::insert(
+		const std::pair<K, V>& entry) {
+	///@todo Finnish this
+	iterator i;
+	for (i = begin(); i != end(); ++i) {
+		if (i->first == entry.first)
+			return std::make_pair(i, (bool) false);
+	}
+	return std::make_pair(i, (bool) false);
 }
 
-// Inserts an element into a map with a specified key value
-// if one with such a key value does not exist.
-// @returns Reference to the value component of the element defined by the key.
-TreeMap::Val& TreeMap::operator[](const Key& k)
-{
-   ///@todo Implement this
-   assert(0);
-   iterator i;
-   return i->second;
+TreeMap::iterator TreeMap::unsafe_insert(const std::pair<K, V>& entry) {
+	///@todo  Finnish this
+	assert(0);
+	return iterator(root);
 }
 
-// Tests if a map is empty.
-bool TreeMap::empty( ) const
-{
-   return root==NULL;
+TreeMap::iterator TreeMap::find(const K& k) {
+	assert(0);
+	return end();
 }
 
-// Returns the number of elements in the map.
-TreeMap::size_type TreeMap::size( ) const
-{
-   ///@todo Implement this
-   assert(0);
-   return 0;
+TreeMap::const_iterator TreeMap::find(const K& k) const {
+	assert(0);
+	return end();
 }
 
-// Returns the number of elements in a map whose key matches a parameter-specified key.
-TreeMap::size_type TreeMap::count(const Key& _Key) const
-{
-   ///@todo Implement this
-   return 1;  // this is not a multimap
+TreeMap::V& TreeMap::operator[](const K& k) {
+	///@todo Implement this
+	assert(0);
+	iterator i;
+	return i->second;
 }
 
-// Removes an element from the map.
-// @returns The iterator that designates the first element remaining beyond any elements removed.
-TreeMap::iterator TreeMap::erase(TreeMap::iterator i)
-{
-   ///@todo Implement this
-   assert(0);
-   return end();
+bool TreeMap::empty() const {
+	return root == NULL;
 }
 
-// Removes a range of elements from the map.
-// The range is defined by the key values of the first and last iterators
-// first is the first element removed and last is the element just beyond the last elemnt removed.
-// @returns The iterator that designates the first element remaining beyond any elements removed.
-TreeMap::iterator TreeMap::erase(TreeMap::iterator f, TreeMap::iterator l)
-{
-   ///@todo Implement this
-   assert(0);
-   return end();
+TreeMap::size_type TreeMap::size() const {
+	///@todo Implement this
+	assert(0);
+	return 0;
 }
 
-// Removes an element from the map.
-// @returns The number of elements that have been removed from the map.
-//          Since this is not a multimap itshould be 1 or 0.
-TreeMap::size_type TreeMap::erase(const Key& key)
-{
-   ///@todo Implement this
-   assert(0);
-   return 0;
+TreeMap::size_type TreeMap::count(const K& _Key) const {
+	return 1;
 }
 
-// Erases all the elements of a map.
-void TreeMap::clear( )
-{
-   TreeMapDetail::erase(this, 0);  
-   ///@todo Implement this
-   assert(0);
+TreeMap::iterator TreeMap::erase(TreeMap::iterator i) {
+	assert(0);
+	return end();
 }
 
-bool TreeMap::struct_eq(const TreeMap& another) const
-{
-   ///@todo Implement this
-   assert(0);
-   return false;
+TreeMap::iterator TreeMap::erase(TreeMap::iterator f, TreeMap::iterator l) {
+	///@todo Implement this
+	assert(0);
+	return end();
 }
 
-bool TreeMap::info_eq(const TreeMap& another) const
-{
-   ///@todo Implement this
-   assert(0);
-   return false;
+TreeMap::size_type TreeMap::erase(const K& key) {
+	///@todo Implement this
+	assert(0);
+	return 0;
+}
+
+void TreeMap::clear() {
+	TreeMapDetail::erase(this, 0);
+	assert(0);
+}
+
+bool TreeMap::struct_eq(const TreeMap& another) const {
+	assert(0);
+	return false;
+}
+
+bool TreeMap::info_eq(const TreeMap& another) const {
+	assert(0);
+	return false;
 }
 
 // preincrement
-TreeMap::const_iterator& TreeMap::const_iterator::operator ++()
-{
-   ///@todo Implement this
-   return *this;
+TreeMap::const_iterator& TreeMap::const_iterator::operator ++() {
+	return *this;
 }
 
 // postincrement
-TreeMap::const_iterator TreeMap::const_iterator::operator++(int)
-{
-   ///@todo Implement this
-   return *this;
+TreeMap::const_iterator TreeMap::const_iterator::operator++(int) {
+	return *this;
 }
 
 // predecrement
-TreeMap::const_iterator& TreeMap::const_iterator::operator--()
-{
-   ///@todo Implement this
-   return *this;
+TreeMap::const_iterator& TreeMap::const_iterator::operator--() {
+	///@todo Implement this
+	return *this;
 }
 
 // postdecrement
-TreeMap::const_iterator TreeMap::const_iterator::operator--(int)
-{
-   ///@todo Implement this
-   return *this;
+TreeMap::const_iterator TreeMap::const_iterator::operator--(int) {
+	return *this;
 }
 
-
-/// Assignment operator copy the source elements into this object.
-TreeMap& TreeMap::operator=(const TreeMap& other)
-{
-   ///@todo Implement this
-   return *this;
-}
-      
-/// Returns an iterator addressing the first element in the map
-TreeMap::iterator TreeMap::begin()
-{ 
-   ///@todo Implement this
-   return iterator(NULL);
+TreeMap& TreeMap::operator=(const TreeMap& other) {
+	return *this;
 }
 
-TreeMap::const_iterator TreeMap::begin() const
-{ 
-   ///@todo Implement this
-   return iterator(NULL);
+TreeMap::iterator TreeMap::begin() {
+	return iterator(NULL);
 }
 
-/// Returns an iterator that addresses the location succeeding the last element in a map
-TreeMap::iterator TreeMap::end()
-{ 
-   ///@todo Implement this
-   return iterator(NULL);
+TreeMap::const_iterator TreeMap::begin() const {
+	return iterator(NULL);
 }
 
-/// Returns an iterator that addresses the location succeeding the last element in a map
-TreeMap::const_iterator TreeMap::end() const
-{ 
-   ///@todo Implement this
-   return iterator(NULL);
+TreeMap::iterator TreeMap::end() {
+	return iterator(NULL);
 }
 
-//////////////////////////////////////////////////////////////////////////////
-// Tests
-//////////////////////////////////////////////////////////////////////////////
-
-/// A helper function that outputs a key-value pair.
-void print(const std::pair<int, std::string>& p)
-{
-   std::cout<<p.first<<", "<<p.second<<std::endl;
+TreeMap::const_iterator TreeMap::end() const {
+	return iterator(NULL);
 }
-
-#include <map>
-
-/// The big mean test function ;)
-void test()
-{
-   // A typedef used by the test.
-   typedef std::map<int, std::string> TEST_MAP;
-   //typedef SmallMap<int, std::string> TEST_MAP;
-   //typedef TreeMap TEST_MAP;
-
-   std::cout << "Testy uzytkownika" << std::endl;
-
-        TEST_MAP m;  
-   
-   m[2] = "Merry";
-   m[4] = "Jane";
-   m[8] = "Korwin";
-   m[4] = "Magdalena";
-
-   for_each(m.begin(), m.end(), print );
-   //system("PAUSE");
-
-}
-
-//////////////////////////////////////////////////////////////////////////////
-// main - jest w pliku /materialy/AISDI/z2/main.cc
-//////////////////////////////////////////////////////////////////////////////
-
-//int main()
-//{
-//   std::cout << "AISDI cwiczenie 4: wchodze do funkcji main." << std::endl;
-
-//   test();
-//   // Biblioteka z bardziej rygorystyczna wersja tych testow bedzie udostepniona na nastepnych zajeciach.
-//   Test2();
-//   //system("PAUSE");
-//   return EXIT_SUCCESS;
-//}
