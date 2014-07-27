4676c81e4c7952a108ee4a776766571ba88ef6b3
Adam Kosiorek <kosiorek.adam@gmail.com>
2014-07-27 15:21:56 +0200

    sorts

--- src/sort/sort.h
+++ src/sort/sort.h
@@ -13,25 +13,29 @@
 #include <vector>
 #include <type_traits>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 template<class T>
-void print(const vector<T>& vec) {
-	for(const auto& el : vec) {
-		cout << el << " ";
+void print(const vector<T>& vec, int i = 0, int j = -1) {
+	if(j == -1) j = vec.size();
+
+	for(int k = 0; k < i; ++k) {
+		cout.width(2);
+		cout << " " << " ";
+	}
+
+	for(; i < j; ++i) {
+		cout.width(2);
+		cout << vec[i] << " ";
 	}
 	cout << endl;
 }
 
-template<class T, template<class > class compare = std::less>
-void Sort(std::vector<T>& vec) {
-	auto comp = compare<T>();
-}
-
-template<class T, template<class > class compare = std::less>
-void BubbleSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
+template<class T, class Comp = less<T>>
+void BubbleSort(vector<T>& vec, Comp comp = Comp()) {
+//	compare<T> comp;
 	{
 
 		for (int i = 0; i < vec.size(); ++i) {
@@ -45,9 +49,9 @@ void BubbleSort(std::vector<T>& vec) {
 	}
 }
 
-template<class T, template<class > class compare = std::less>
-void SelectionSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
+template<class T, template<class > class compare = less>
+void SelectionSort(vector<T>& vec) {
+	compare<T> comp;
 
 	for (int i = 0; i < vec.size(); ++i) {
 
@@ -61,9 +65,9 @@ void SelectionSort(std::vector<T>& vec) {
 	}
 }
 
-template<class T, template<class > class compare = std::less>
-void InsertionSort(std::vector<T>& vec, int inc = 1) {
-	auto comp = compare<T>();
+template<class T, template<class > class compare = less>
+void InsertionSort(vector<T>& vec, int inc = 1) {
+	compare<T> comp;
 
 	for (int i = inc; i < vec.size(); i += inc) {
 		int j = i - inc;
@@ -80,10 +84,9 @@ void InsertionSort(std::vector<T>& vec, int inc = 1) {
 	}
 }
 
-template<class T, template<class > class compare = std::less>
-void ShellSort(std::vector<T>& vec,
+template<class T, template<class > class compare = less>
+void ShellSort(vector<T>& vec,
 		const vector<int>& increments = { 5, 3, 1 }) {
-	auto comp = compare<T>();
 
 	auto inc = begin(increments);
 	while (*inc > vec.size()) {
@@ -94,29 +97,140 @@ void ShellSort(std::vector<T>& vec,
 	}
 }
 
-template<class T, template<class > class compare = std::less>
-void MergeSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
-}
-;
+template<class T, class Comp>
+void mergeCombine(const Comp& comp, vector<T>& vec, vector<T>& aux, int low, int mid, int high) {
+	int i = low, j = mid, l = low;
+	while(l < high) {
 
-template<class T, template<class > class compare = std::less>
-void QuickSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
-}
-
-template<class T, template<class > class compare = std::less>
-void HeapSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
+		if(i >= mid) aux[l++] = vec[j++];
+		else if(j >= high) aux[l++] = vec[i++];
+		else if(comp(vec[i], vec[j])) aux[l++] = vec[i++];
+		else aux[l++] = vec[j++];
+	}
 }
 
-template<class T, template<class > class compare = std::less>
-void CountSort(std::vector<T>& vec, function<int(const T&)> mapping) {
-	auto comp = compare<T>();
+template<class T, class Comp>
+void mergeSortImpl(const Comp& comp, vector<T>& vec, vector<T>& aux, int low, int high) {
+
+	if(high - low < 2) {
+		return;
+	}
+
+	const int mid = (high - low + 1) / 2 + low;
+	//	swap vec and aux - saves some time due to implicit coping
+	mergeSortImpl(comp, aux, vec, low, mid);
+	mergeSortImpl(comp, aux, vec, mid, high);
+
+	mergeCombine(comp, vec, aux, low, mid, high);
+}
+
+template<class T, template<class > class compare = less>
+void MergeSort(vector<T>& vec, compare<T> comp = compare<T>()) {
+
+	vector<T> aux(vec);
+	mergeSortImpl(comp, vec, aux, 0, vec.size());
+	vec = aux;
+}
+
+template<class T, class Comp>
+void MergeSortComp(vector<T>& vec, Comp comp) {
+
+	vector<T> aux(vec);
+	mergeSortImpl(comp, vec, aux, 0, vec.size());
+	vec = aux;
+}
+
+
+int quickChoosePivot(int low, int high) {
+
+	return (rand() % (high - low + 1)) + low;
+}
+
+template<class T, class Comp>
+int quickPartition(const Comp& comp, vector<T>& vec, int low, int high, int pivot = -1) {
+
+	if(pivot == -1) {
+		pivot = quickChoosePivot(low, high);
+	}
+
+	swap(vec[pivot], vec[high]);
+	pivot = high;
+	int i = low, j = high - 1;
+	while(i < j) {
+		if(comp(vec[i], vec[pivot])) ++i;
+		else if(comp(vec[pivot], vec[j])) --j;
+		else swap(vec[i++], vec[j--]);
+	}
+	while(comp(vec[j], vec[pivot])) {
+		if(j < high) ++j;
+	}
+	swap(vec[j], vec[pivot]);
+	return j;
+}
+
+template<class T, class Comp>
+void QuickSortImpl(const Comp& comp, vector<T>& vec, int low, int high) {
+	if(high - low + 1 <= 1) {
+		return;
+	}
+
+	int pivot = quickPartition(comp, vec, low, high);
+	QuickSortImpl(comp, vec, low, pivot - 1);
+	QuickSortImpl(comp, vec, pivot + 1, high);
+}
+
+
+template<class T, template<class > class compare = less>
+void QuickSort(vector<T>& vec) {
+	const compare<T> comp;
+
+	QuickSortImpl(comp, vec, 0, vec.size() - 1);
+}
+
+template<class T, class Comp>
+void siftDown(const Comp& comp, vector<T>& vec, int start, int end) {
+
+	while(2 * start + 1 < end) {
+		int swap = 2 * start + 1;
+		if(swap + 1 < end && comp(vec[swap + 1], vec[swap])) {
+			++swap;
+		}
+		if(comp(vec[swap], vec[start])) {
+			std::swap(vec[start], vec[swap]);
+		} else {
+			return;
+		}
+		start = swap;
+	}
+}
+
+template<class T, class Comp>
+void heapify(const Comp& comp, vector<T>& vec) {
+
+	for(int i = vec.size() / 2 - 1; i >= 0; --i) {
+		siftDown(comp, vec, i, vec.size());
+	}
+}
+
+template<class T, template<class > class compare = less>
+void HeapSort(vector<T>& vec) {
+	compare<T> comp;
+	heapify(comp, vec);
+
+	for(int i = vec.size() - 1; i > 0; --i) {
+		swap(vec[0], vec[i]);
+		siftDown(comp, vec, 0, i);
+	}
+	std::reverse(begin(vec), end(vec));
+}
+
+template<class T, template<class > class compare = less>
+void CountSort(vector<T>& vec, function<int(const T&)> mapping) {
+	compare<T> comp;
 
 	int max = *max_element(begin(vec), end(vec), comp);
 	int min = *min_element(begin(vec), end(vec), comp);
-	std::vector<int> countVec(max - min + 1, 0);
+	vector<int> countVec(max - min + 1, 0);
 	auto it = begin(vec);
 	auto endIt = end(vec);
 	while (it != endIt) {
@@ -131,7 +245,7 @@ void CountSort(std::vector<T>& vec, function<int(const T&)> mapping) {
 	}
 	countVec[0] = 0;
 
-	std::vector<T> sortedVec(vec.size());
+	vector<T> sortedVec(vec.size());
 	it = begin(vec);
 	while (it != endIt) {
 		int index = mapping(*it) - min;
@@ -141,14 +255,54 @@ void CountSort(std::vector<T>& vec, function<int(const T&)> mapping) {
 	vec = sortedVec;
 }
 
-template<class T, template<class > class compare = std::less>
-void BucketSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
+template<class T>
+void BucketSort(vector<T>& vec, int bins) {
+	static_assert(is_arithmetic<T>::value, "Implemented for arithmetics types only");
+
+	bins += 1;
+	int* counter = new int[bins];
+	memset(counter, 0, bins * sizeof(int));
+	T min = *min_element(begin(vec), end(vec));
+	T max = *max_element(begin(vec), end(vec));
+	T binSize = (max - min) / (bins - 1);
+
+	for_each(begin(vec), end(vec), [counter, binSize](T t) { ++counter[int(t / binSize)];});
+	transform(counter, counter + bins - 1, counter + 1, counter + 1, [](int i1, int i2)
+			{ return i1 + i2;} );
+
+	rotate(counter, counter + bins - 1, counter + bins);
+	counter[0] = 0;
+
+	vector<T> aux(vec.size());
+	for_each(begin(vec), end(vec), [&aux, binSize, counter](T t) {
+
+		aux[counter[int(t / binSize)]++] = t;
+	});
+	vec = aux;
+
+	delete[] counter;
 }
 
-template<class T, template<class > class compare = std::less>
-void RadixSort(std::vector<T>& vec) {
-	auto comp = compare<T>();
+template<template<class > class compare = less>
+void RadixSort(vector<string>& vec) {
+	compare<char> comp;
+
+	int length = max_element(begin(vec), end(vec),
+			[](const string& s1, const string& s2) {
+		return s1.length() < s2.length();
+	})->length();
+
+	for(int i = length; i > 0; --i) {
+		auto comparator = [&comp, i](const string& s1, const string& s2) {
+
+
+			if(s1.length() >= i && s2.length() >= i) return comp(s1[i - 1], s2[i - 1]);
+			return s1.length() < s2.length();
+		};
+
+		BubbleSort(vec, comparator);
+	}
+
 }
 
 #endif /* SORT_H_ */
--- src/sort/test.cpp
+++ src/sort/test.cpp
@@ -18,30 +18,37 @@ using namespace std;
 
 class SortTest : public ::testing::Test {
 public:
-  SortTest() : seed_(0), size_(100), randomVec(0) {
+  SortTest() : seed_(0), size_(100), randomVec(size_),
+  	orderedVec(size_), reversedVec(size_) {
 
     srand(seed_);
-    randomVec.resize(size_);
     generate(begin(randomVec), end(randomVec), [this]() { return rand() % size_; });
-
+    for(int i = 0; i < size_; ++i) {
+    	orderedVec[i] = i;
+    	reversedVec[i] = size_ - i - 1;
+    }
   };
   ~SortTest() {};
 
   const int seed_;
   const int size_;
-  std::vector<int> randomVec;
+  vector<int> randomVec;
+  vector<int> orderedVec;
+  vector<int> reversedVec;
 
-  void print() {
-	  for(const auto& el : randomVec) {
+  template<class T>
+  void print(const vector<T>& vec) {
+	  for(const auto& el : vec) {
 		  cout << el << " ";
 	  }
 	  cout << endl;
   }
 
-  void printSorted() {
-	  auto vec(randomVec);
-	  sort(begin(vec), end(vec));
-	  for(const auto& el : vec) {
+  template<class T>
+  void printSorted(const vector<T>& vec) {
+	  auto v(vec);
+	  sort(begin(v), end(v));
+	  for(const auto& el : v) {
 	  		  cout << el << " ";
 	  	  }
 	  	  cout << endl;
@@ -51,49 +58,160 @@ public:
 TEST_F(SortTest, Bubble) {
   BubbleSort(randomVec);
   ASSERT_SORTED(randomVec);
+  BubbleSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  BubbleSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
 }
 
 TEST_F(SortTest, Selection) {
   SelectionSort(randomVec);
   ASSERT_SORTED(randomVec);
+  SelectionSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  SelectionSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
 }
 
 TEST_F(SortTest, Insertion) {
   InsertionSort(randomVec);
   ASSERT_SORTED(randomVec);
+  InsertionSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  InsertionSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
 }
 
 TEST_F(SortTest, Shell) {
   ShellSort(randomVec);
   ASSERT_SORTED(randomVec);
+  ShellSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  ShellSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
 }
 
-//TEST_F(SortTest, Merge) {
-//  MergeSort(randomVec);
-//  ASSERT_SORTED(randomVec);
-//}
+TEST_F(SortTest, Merge) {
+  MergeSort(randomVec);
+  ASSERT_SORTED(randomVec);
+  MergeSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  MergeSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
+}
 
-TEST_F(SortTest, DISABLED_Quick) {
-	print();
-  QuickSort(randomVec);
-  print();
-  printSorted();
+TEST_F(SortTest, QuickChoosePivot) {
+
+	for(int i = 0; i < 1000; ++i) {
+		int low = rand() % 1000000;
+		int high = rand() % 1000000;
+		if(low > high) swap(low, high);
+		int pivot = quickChoosePivot(low, high);
+		ASSERT_GE(pivot, low);
+		ASSERT_LE(pivot, high);
+	}
+}
+
+TEST_F(SortTest, QuickPartition) {
+
+	less<int> comp;
+	for(uint p = 0; p < randomVec.size(); ++p) {
+		int pivotValue = randomVec[p];
+		int pivotIndex = quickPartition(comp, randomVec, 0, randomVec.size() - 1, p);
+		ASSERT_EQ(randomVec[pivotIndex], pivotValue) << "p = " << p;
+		for(int i = 0; i < pivotIndex; ++i) {
+			ASSERT_LE(randomVec[i], pivotValue);
+		}
+		for(uint i = pivotIndex + 1; i < randomVec.size(); ++i) {
+			ASSERT_GE(randomVec[i], pivotValue) << "i = " << i << " p = " << p;
+		}
+	}
+}
+
+TEST_F(SortTest, Quick) {
+  QuickSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
+  QuickSort(orderedVec);
+  ASSERT_SORTED(orderedVec);
+  QuickSort(reversedVec);
+  ASSERT_SORTED(reversedVec);
+}
+
+TEST_F(SortTest, HeapSiftDown) {
+
+	less<int> comp;
+	vector<int> vec = {5, 4, 3, 2, 1};
+	siftDown(comp, vec, 0, vec.size());
+	ASSERT_EQ(vec[0], 3);
+	ASSERT_EQ(vec[1], 4);
+	ASSERT_EQ(vec[2], 5);
+	ASSERT_EQ(vec[3], 2);
+	ASSERT_EQ(vec[4], 1);
+}
+
+TEST_F(SortTest, HeapHeapify) {
+
+	less<int> comp;
+	vector<int> vec = {5, 4, 3, 2, 1};
+	heapify(comp, vec);
+	ASSERT_EQ(vec[0], 1);
+	ASSERT_EQ(vec[1], 2);
+	ASSERT_EQ(vec[2], 3);
+	ASSERT_EQ(vec[3], 5);
+	ASSERT_EQ(vec[4], 4);
+}
+
+TEST_F(SortTest, Heap) {
+  HeapSort(randomVec);
   ASSERT_SORTED(randomVec);
 }
-
-//TEST_F(SortTest, Heap) {
-//  HeapSort(randomVec);
-//  ASSERT_SORTED(randomVec);
-//}
 
 TEST_F(SortTest, Count) {
 function<int(const int&)> fun = [](const int& t) {return static_cast<int>(t);};
   CountSort(randomVec, fun);
   ASSERT_SORTED(randomVec);
+  CountSort(orderedVec, fun);
+  ASSERT_SORTED(orderedVec);
+  CountSort(reversedVec, fun);
+  ASSERT_SORTED(reversedVec);
 }
 
-//Count
-//Bucket
-//Radix
+TEST_F(SortTest, Bucket) {
+	vector<float> randomVec(begin(this->randomVec), end(this->randomVec));
+	transform(begin(randomVec), end(randomVec), begin(randomVec), [](float f) {return f * 0.01;});
+	BucketSort(randomVec, 10);
+	float min = *min_element(begin(randomVec), end(randomVec));
+	float max = *max_element(begin(randomVec), end(randomVec));
+	float bucketSize = (max - min) / 10;
+	auto first = begin(randomVec);
+	auto second = ++begin(randomVec);
+	auto end = std::end(randomVec);
+	while(second < end) {
+		ASSERT_GT(*second++, *first++ - bucketSize);
+	}
+}
+
+static string randomStrGen(int length) {
+    static string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    string result;
+    result.resize(length);
+
+    for (int i = 0; i < length; i++)
+        result[i] = charset[rand() % charset.length()];
+
+    return result;
+}
+
+TEST_F(SortTest, Radix) {
+	int N = 100;
+	vector<string> vec(N);
+	generate_n(begin(vec), 100, [](){ return randomStrGen((rand() % 10) + 1);});
+	print(vec);
+	RadixSort(vec);
+	print(vec);
+	printSorted(vec);
+	ASSERT_SORTED(vec);
+
+}
 
 
